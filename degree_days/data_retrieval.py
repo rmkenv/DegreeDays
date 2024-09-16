09d4e7611bdb1b28c5639629d75db6d0b6bc38be
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2024-09-16 08:16:23 -0400

    Update data_retrieval.py

--- degree_days/data_retrieval.py
+++ degree_days/data_retrieval.py
@@ -4,30 +4,29 @@ import pandas as pd
 from meteostat import Daily, Point
 
 def fetch_temperature_data(start_date, end_date, latitude, longitude):
-    """
-    Fetch daily temperature data from Meteostat for a given location and time period.
-    
-    Parameters:
-    - start_date: str or datetime, start of the period
-    - end_date: str or datetime, end of the period
-    - latitude: float, latitude of the location
-    - longitude: float, longitude of the location
-    
-    Returns:
-    - pandas DataFrame with temperature data
-    """
-    
-    api_key = os.getenv('METEOSTAT_API_KEY')
-    
-    if not api_key:
-        raise ValueError("You must set your Meteostat API key as an environment variable 'METEOSTAT_API_KEY'.")
+  """
+  Fetch daily temperature data from Meteostat for a given location and time period.
+  
+  Parameters:
+  - start_date: str or datetime, start of the period
+  - end_date: str or datetime, end of the period
+  - latitude: float, latitude of the location
+  - longitude: float, longitude of the location
+  
+  Returns:
+  - pandas DataFrame with temperature data
+  """
+  
+  api_key = os.getenv('METEOSTAT_API_KEY')
+  
+  if not api_key:
+      raise ValueError("You must set your Meteostat API key as an environment variable 'METEOSTAT_API_KEY'.")
 
-    # Meteostat now uses the API key automatically if it's set as an environment variable
-    # So we don't need to explicitly set it
+  # Meteostat now uses the API key automatically if it's set as an environment variable
 
-    location = Point(latitude, longitude)
-    data = Daily(location, start_date, end_date)
-    data = data.fetch()
+  location = Point(latitude, longitude)
+  data = Daily(location, start_date, end_date)
+  data = data.fetch()
 
-    # Return relevant temperature columns (tavg, tmin, tmax)
-    return data[['tavg', 'tmin', 'tmax']]
+  # Return relevant temperature columns (tavg, tmin, tmax)
+  return data[['tavg', 'tmin', 'tmax']]
