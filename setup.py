2f098219d8abde30117eb5f3d8a3c7f6947cfca5
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2024-09-16 07:55:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='degree_days',
     version='0.1.0',
     author='Ryan Kmetz',
-    author_email='kmetzrm@gmail.com,
+    author_email='kmetzrm@gmail.com',  # Fixed missing closing quote
     description='A library for calculating Heating and Cooling Degree Days for energy analysis.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/degree_days',
+    url='https://github.com/yourusername/degree_days',  # You might want to update this URL
     packages=find_packages(),
     install_requires=[
         'pandas>=1.1',
@@ -17,10 +17,29 @@ setup(
         'matplotlib>=3.3',
         'meteostat>=1.5'
     ],
+    extras_require={
+        'dev': [  # Added a 'dev' extra for development dependencies
+            'pytest',
+            'flake8',
+        ],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',  # Added specific Python version classifiers
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3.6',
+    include_package_data=True,  # Added to include non-Python files if any
+    entry_points={  # Added in case you want to provide command-line scripts
+        'console_scripts': [
+            'degree_days=degree_days.cli:main',  # Adjust as needed
+        ],
+    },
 )
+
+# Created/Modified files during execution:
+print("setup.py")
